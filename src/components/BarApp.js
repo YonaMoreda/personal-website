f0210a5a3679c2e3025c7b2380b1c80b5ab1162e
YonaMoreda <yokots3q@gmail.com>
2020-07-28 12:46:40 +0200

    fixed/improved stylization

--- src/components/BarApp.js
+++ src/components/BarApp.js
@@ -13,41 +13,41 @@ class BarApp extends Component {
                         "id": 1,
                         "label": "Software engineering",
                         "value": 95,
-                        "color": ["#01579B", "#E1F5FE"]
+                        "color": ["#01579B", "#023046"]
                     },
                     {
                         "id": 2,
                         "label": "Linear Algebra and Multivariable Calculus",
                         "value": 95,
-                        "color": ["#01579B", "#E1F5FE"]
+                        "color": ["#01579B", "#023046"]
                     },
 
                     {
                         "id": 3,
                         "label": "Short Programming Project",
                         "value": 90,
-                        "color": ["#01579B", "#E1F5FE"]
+                        "color": ["#01579B", "#023046"]
                     },
 
                     {
                         "id": 4,
                         "label": "Advanced Algorithms and Data Structures",
                         "value": 90,
-                        "color": ["#01579B", "#E1F5FE"]
+                        "color": ["#01579B", "#023046"]
                     },
 
                     {
                         "id": 5,
                         "label": "Research Skills in Computing Science",
                         "value": 90,
-                        "color": ["#01579B", "#E1F5FE"]
+                        "color": ["#01579B", "#023046"]
                     },
 
                     {
                         "id": 6,
                         "label": "Discrete Structures",
                         "value": 90,
-                        "color": ["#01579B", "#E1F5FE"]
+                        "color": ["#01579B", "#023046"]
                     }
                 ]
             }
