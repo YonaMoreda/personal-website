83ba2eb7291d0700f36cbdd3527534d64556a08b
YonaMoreda <yokots3q@gmail.com>
2020-07-29 11:51:15 +0200

    fixed/improved stylization Contact.css

--- src/components/ContactsApp.js
+++ src/components/ContactsApp.js
@@ -4,16 +4,30 @@ import '../StyleSheets/Contact.css'
 function ContactsApp() {
     return (
         <div className="Contacts">
+
+
             <h2>Contact Me</h2>
-            <h4>Linkedin</h4>
-            {/* eslint-disable-next-line react/jsx-no-target-blank */}
-            <a target="_blank" href="https://www.linkedin.com/in/yona-moreda-116154141/">@yona-moreda</a>
-            <h4>Github</h4>
-            {/* eslint-disable-next-line react/jsx-no-target-blank */}
-            <a target="_blank" href="https://github.com/YonaMoreda">@YonaMoreda</a>
-            <h4>E-Mail</h4>
-            {/* eslint-disable-next-line react/jsx-no-target-blank */}
-            <a target="_blank" href="mailto:yokots3q@gmail.com">yokots3q@gmail.com</a>
+            <table>
+                <tr>
+                    <th>Linkedin</th>
+                    <th>Github</th>
+                    <th>E-Mail</th>
+                </tr>
+                <tr>
+                    <td>
+                        {/* eslint-disable-next-line react/jsx-no-target-blank */}
+                        <a target="_blank" href="https://www.linkedin.com/in/yona-moreda-116154141/">@yona-moreda</a>
+                    </td>
+                    <td>
+                        {/* eslint-disable-next-line react/jsx-no-target-blank */}
+                        <a target="_blank" href="https://github.com/YonaMoreda">@YonaMoreda</a>
+                    </td>
+                    <td>
+                        {/* eslint-disable-next-line react/jsx-no-target-blank */}
+                        <a target="_blank" href="mailto:yokots3q@gmail.com">yokots3q@gmail.com</a>
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 }
